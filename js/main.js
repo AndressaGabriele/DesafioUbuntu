edf9c04d6bc0e3a822b6551f18b4a6b7b71daab3
ANDRESSA CHAGAS <andressa.chagas79@gmail.com>
2023-03-29 17:40:30 -0300

    Implementaçao do style

--- js/main.js
+++ js/main.js
@@ -1,4 +1,57 @@
-console.log("conectado")
+// console.log("conectado")
+// const leftBtn = document.getElementById("left");
+// const centerBtn = document.getElementById("center");
+// const rightBtn = document.getElementById("right");
+// const slider = document.getElementById("slider");
+
+// let trackPosition = 2;
+
+// const changeOrder= (e)=>{
+//   let direction = e.target.id;
+//   switch (direction) {
+//     case "left":
+//       if (trackPosition === 3) {
+//         slider.style.animationName = "rightToLeft";
+//         slider.style.animationDuration = "0.5s";
+//         trackPosition = 1;
+//         return (slider.style.left = "0vw");
+//       }
+//       trackPosition = 1;
+//       slider.style.animationName = "left";
+//       slider.style.animationDuration = "1s";
+//       return (slider.style.left = "0px");
+
+//     case "center": {
+//       if (trackPosition > 2) {
+//         slider.style.animationName = "rightToCenter";
+//         slider.style.animationDuration = "1s";
+//       } else if (trackPosition < 2) {
+//         slider.style.animationName = "leftToCenter";
+//         slider.style.animationDuration = "1s";
+//       }
+//       trackPosition = 2;
+//       return (slider.style.left = "-15vw");
+//     }
+//     case "right":
+//       if (trackPosition === 1) {
+//         slider.style.animationName = "leftToRight";
+//         slider.style.animationDuration = "0.5s";
+//         trackPosition = 3;
+//         return (slider.style.left = "-30vw");
+//       }
+//       trackPosition = 3;
+//       slider.style.animationName = "right";
+//       slider.style.animationDuration = "1s";
+//       return (slider.style.left = "-30vw");
+//   }
+// };
+
+// leftBtn.addEventListener("click", changeOrder);
+// centerBtn.addEventListener("click", changeOrder);
+// rightBtn.addEventListener("click", changeOrder);
+
+// MEDIC 
+
 const leftBtn = document.getElementById("left");
 const centerBtn = document.getElementById("center");
 const rightBtn = document.getElementById("right");
@@ -6,7 +59,7 @@ const slider = document.getElementById("slider");
 
 let trackPosition = 2;
 
-const changeOrder= (e)=>{
+const changeOrder = (e) => {
   let direction = e.target.id;
   switch (direction) {
     case "left":
@@ -30,19 +83,19 @@ const changeOrder= (e)=>{
         slider.style.animationDuration = "1s";
       }
       trackPosition = 2;
-      return (slider.style.left = "-15vw");
+      return (slider.style.left = "-21.5vw");
     }
     case "right":
       if (trackPosition === 1) {
         slider.style.animationName = "leftToRight";
         slider.style.animationDuration = "0.5s";
         trackPosition = 3;
-        return (slider.style.left = "-30vw");
+        return (slider.style.left = "-43vw");
       }
       trackPosition = 3;
       slider.style.animationName = "right";
       slider.style.animationDuration = "1s";
-      return (slider.style.left = "-30vw");
+      return (slider.style.left = "-43vw");
   }
 };
 
